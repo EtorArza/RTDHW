24cb39cbe6842aff56da7ed692160c1b64a0e783
EtorArza <etorarza@gmail.com>
2020-07-09 18:22:07 +0000

    results celeron N4100 & bugfixes

--- launch.sh
+++ launch.sh
@@ -7,6 +7,10 @@ if [[ "$#" -ne 1  ]] ; then
     exit 1
 fi
 
+git config --global user.email "etorarza@gmail.com"
+git config --global user.name "EtorArza"
+
+
 cpu_name=$1
 
 sudo apt install -y g++
