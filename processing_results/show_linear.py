d6f73dde4965b84d852c4ac9e72cff3b35b95ad6
EtorArza <etorarza@gmail.com>
2020-07-10 21:08:26 +0200

    delete thiinkpad270x results to reexecute without power saving mode

--- processing_results/show_linear.py
+++ processing_results/show_linear.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from matplotlib import pyplot as plt
 
 columns=["cpuname","problemtype","problempath","methodname","operatorname","nevals","maxtime","maxevals","time","evals","fitness"]
 
@@ -17,15 +17,32 @@ df = pd.DataFrame(lines, columns=columns)
 cpunames = df["cpuname"].unique()
 
 sub_df_0 = df[(df["operatorname"]=="exchange") & (df["problempath"]=="instances/qap/tai100a.qap")].reset_index(drop=True)
-sub_df_1 = df[(df["operatorname"]=="swap") & (df["problempath"]=="instances/lop/N-p40-02.lop")].reset_index(drop=True)
+sub_df_1 = df[(df["operatorname"]=="swap") & (df["problempath"]=="instances/lop/N-t65d11xx_150.lop")].reset_index(drop=True)
 sub_df_2 = df[(df["operatorname"]=="insert") & (df["problempath"]=="instances/pfsp/tai200_20_0.pfsp")].reset_index(drop=True)
-sub_df_3 = df[(df["operatorname"]=="random_search") & (df["problempath"]=="instances/tsp/kroD100.tsp")].reset_index(drop=True)
+sub_df_3 = df[(df["operatorname"]=="random_search") & (df["problempath"]=="instances/tsp/kroB200.tsp")].reset_index(drop=True)
 
 
 list_of_subdf = [sub_df_0, sub_df_1, sub_df_2, sub_df_3]
 
 res_df = sub_df_0["time"]/sub_df_1["time"]
 
-print(res_df)
+x = 0
+for i in range(len(list_of_subdf)):
+    for j in range(len(list_of_subdf)):
+        if i >= j:
+            continue
+        res_df = list_of_subdf[i]["time"]/list_of_subdf[j]["time"]
+        x_pos = [x for _ in range(len(list_of_subdf[0]))]
+        plt.scatter(x_pos, res_df, marker=".")
+        for y,cpuname in zip(res_df, cpunames):
+            plt.text(x_pos[0], y, cpuname + "  " + str((i,j)))
+        x += 1
+
+
+        
+
+plt.show()
+
+print(list_of_subdf)
 print("------")
 print(sub_df_0["time"],sub_df_1["time"])
